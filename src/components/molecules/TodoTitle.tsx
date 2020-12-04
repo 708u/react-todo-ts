045a3a8f4edebd53f6dc024dacae957544499c13
708u <naoya.uda@showcase-gig.com>
2020-12-05 07:34:12 +0900

    feat: implement input todo

--- src/components/molecules/TodoTitle.tsx
+++ src/components/molecules/TodoTitle.tsx
@@ -1,10 +1,19 @@
-import { FC } from 'react';
+import Input from 'components/atoms/Input';
+import { Dispatch, FC } from 'react';
 
-const TodoTitle: FC = () => {
+type Props = {
+  newTodo: string;
+  setTodo: Dispatch<string>;
+};
+
+const TodoTitle: FC<Props> = (props) => {
+  const { newTodo, setTodo } = props;
   return (
     <header>
       <h1>todos</h1>
-      <input className="new-todo" placeholder="What needs to be done?" />
+      <form id="todo-addition-form" onSubmit={(e) => e.preventDefault()}>
+        <Input value={newTodo} placeholder="What needs to be done?" onChange={(e) => setTodo(e.target.value)} />
+      </form>
     </header>
   );
 };
--- src/components/organisms/Todo.tsx
+++ src/components/organisms/Todo.tsx
@@ -1,12 +1,13 @@
 import TodoFooter from 'components/molecules/TodoFooter';
 import TodoList from 'components/molecules/TodoList';
 import TodoTitle from 'components/molecules/TodoTitle';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 
 const Todo: FC = () => {
+  const [newTodo, setTodo] = useState('');
   return (
     <section className="todoapp">
-      <TodoTitle />
+      <TodoTitle newTodo={newTodo} setTodo={setTodo} />
       <TodoList />
       <TodoFooter />
     </section>
